183fdb525ed05f47a04f0fd7d516c2f721bea6f4
austin li <645267294@qq.com>
2024-11-20 15:39:08 -0500

    fix

---
+++ controllers/foods.js
@@ -0,0 +1,41 @@
+// controllers/foods.js
+
+const express = require("express");
+const router = express.Router();
+
+const User = require("../models/user.js");
+
+// router logic will go here - will be built later on in the lab
+router.get("/", (req, res) => {
+  res.render("foods/index.ejs");
+});
+
+router.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+
+
+// create post
+router.post("/", async (req, res) => {
+  try {
+    console.log(111);
+    const { _id }= req.session.user;
+    console.log(req.session.user);
+    const todoUser = await User.findOne({ _id: _id });
+    console.log(todoUser);
+    const pantryData = {
+      name: req.body
+    };
+    todoUser.pantry.push(pantryData);
+    await todoUser.save();
+    console.log(todoUser);
+    res.status(200).redirect(`/users/${req.session.user._id}/foods`);
+
+  } catch (error) {
+    console.error(error);
+    res.status(418).redirect("/");
+  }
+});
+
+module.exports = router;
--- models/user.js
+++ models/user.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
+const foodSchema = new mongoose.Schema({
+  // YOU DO: Define properties of food schema
+  name:{
+    type:String,
+    require:true
+  }
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +17,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  pantry: [foodSchema]
 });
 
 const User = mongoose.model('User', userSchema);
