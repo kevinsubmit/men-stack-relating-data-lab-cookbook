6655fd97a0548418985921871984e76598a75f92
austin li <645267294@qq.com>
2024-11-25 09:14:26 -0500

    remover no-needed item

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const passUserToView = require("./middleware/pass-user-to-view.js");
 
 const authController = require("./controllers/auth.js");
 const foodsController = require("./controllers/foods.js");
-
 const usersController = require("./controllers/users.js");
 
 const port = process.env.PORT ? process.env.PORT : "3000";
@@ -40,15 +39,6 @@ app.get("/", (req, res) => {
     user: req.session.user,
   });
 });
-
-app.get("/vip-lounge", (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send("Sorry, no guests allowed.");
-  }
-});
-
 
 app.use(passUserToView);
 app.use("/auth", authController);
